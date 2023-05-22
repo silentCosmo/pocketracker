d88d3142b72735a17c0434941b7f82609683a92a
DevCosmo <silentcosmox@gmail.com>
2023-05-22 21:36:47 +0530

    nt_fill

--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -16,18 +16,18 @@ function NewTransaction() {
         <p className='uppercase text-sm bg-teal-600 p-1 text-slate-900 font-medium cursor-none'>New Transaction</p>
         <div className='mt-2'>
         <form action="">
-            <input type="date" name="" id="" className='focus:border-t-teal-500 border border-slate-800 bg-slate-900 outline-none text-center w-36' />
-            <ul className="flex flex-row p-1 mt-2 border border-slate-800 bg-slate-900 list-none hover:border-t-teal-500">
-              <li>
+            <ul className="flex flex-row gap-1 p-1 mt-2 justify-center border border-slate-800 bg-slate-900 list-none hover:border-t-teal-500">
+              <li className='flex w-full'>
               <input type="radio" name="type" id="income" className='hidden peer' onChange={handleTransactionType} defaultChecked/>
-              <label htmlFor="income" className='p-0.5 peer-checked:bg-teal-600 peer-checked:text-emerald-950 bg-slate-950 border border-slate-900'>&nbsp;Income&nbsp;</label>
+              <label htmlFor="income" className='py-0.5 w-full block peer-checked:bg-teal-950 peer-checked:text-teal-300 peer-checked:border-2 peer-checked:border-teal-500 bg-slate-950 border border-slate-900'>&nbsp;Income&nbsp;</label>
               </li>
-              <li>
+            <li className='flex w-full'>
                 <input type="radio" name="type" id="expense" className='hidden peer' onChange={handleTransactionType}/>
-                <label htmlFor="expense" className='peer-checked:bg-teal-600 peer-checked:text-pink-950 bg-slate-950 border border-slate-900'>&nbsp;Expense</label>
+                <label htmlFor="expense" className='py-0.5 w-full block peer-checked:bg-pink-950 peer-checked:text-pink-300 peer-checked:border-2 peer-checked:border-pink-800 bg-slate-950 border border-slate-900'>&nbsp;Expense&nbsp;</label>
               </li>
             </ul>
-            <select name="category" className='focus:border-t-teal-500 border border-slate-800 bg-slate-900 mt-2 w-full outline-none overflow-x-visible'>
+            <input type="date" name="" id="" className='mt-2 p-0.5 pl-4 focus:border-t-teal-500 border border-slate-800 bg-slate-900 outline-none text-center w-full' required />
+            <select name="category" className='p-1 pl-4 text-center focus:border-t-teal-500 border border-slate-800 bg-slate-900 mt-2 w-full outline-none overflow-x-visible' required>
                 <option value="" disabled selected>Category</option>
                 { isInc ?
                 <>
@@ -50,8 +50,8 @@ function NewTransaction() {
                 </>
                 }
             </select>
-            <input type="number" name="" id="" className='focus:border-b-teal-500 border border-slate-800 bg-slate-900 outline-none text-center w-36 mt-2 appearance-none' placeholder='Amount'/>
-            <input type="submit" value="Create" className='bg-teal-600 text-slate-900 mt-2 w-full hover:bg-teal-500 active:bg-teal-400'/>
+            <input type="number" name="" id="" className='p-0.5 focus:border-b-teal-500 border border-slate-800 bg-slate-900 outline-none text-center w-full mt-2 appearance-none' placeholder='Amount' required/>
+            <input type="submit" value="Create" className='p-0.5 bg-teal-600 text-slate-900 mt-2 w-full hover:bg-teal-500 active:bg-teal-400'/>
         </form>
         </div>
     </div>
