6307f2c2ed96d19575f0e3f37d665d64e8061177
DevCosmo <silentcosmox@gmail.com>
2023-05-23 03:14:35 +0530

    _redirects

--- src/components/History.js
+++ src/components/History.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import TransactionDetails from './analytics/TransactionDetails'
 import DeleteModal from './analytics/DeleteModal'
 
 function History() {
-  const [del,setDel] = useState(false)
-  const handleClick=()=>{setDel(true); console.log('clicked');}
   return (
     <div className='px-2 mb-4'>
         <p className='pb-1 bg-slate-900 text-teal-600 uppercase'>History</p>
@@ -13,8 +11,7 @@ function History() {
         <TransactionDetails details={{ inc:false,cat:'Entertaiment',amt:'456'}} />
         <TransactionDetails details={{ inc:false,cat:'shopping',amt:'556'}} />
         <TransactionDetails details={{ inc:true,cat:'gift',amt:'38'}} />
-        <div onClick={handleClick} className='bg-red-500'>
-        <TransactionDetails details={{ inc:false,cat:'Health',amt:'256'}} /></div>
+        <TransactionDetails details={{ inc:false,cat:'Health',amt:'256'}} />
         
         <TransactionDetails details={{ inc:false,cat:'Health',amt:'256'}} />
         <TransactionDetails details={{ inc:false,cat:'Health',amt:'256'}} />
@@ -32,7 +29,7 @@ function History() {
         <TransactionDetails details={{ inc:false,cat:'Health',amt:'256'}} />
         </div>
         <p className='opacity-30 mt-1'>&lt; Swipe left to delete</p>
-        {del?<DeleteModal/>:""}
+        
         {/* <p className='mt-20 opacity-60'>No Transactions!</p> */}
         
     </div>
