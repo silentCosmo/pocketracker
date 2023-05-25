408312a74acd366ae1d7f9b1cf9e6869e9d19a1c
DevCosmo <silentcosmox@gmail.com>
2023-05-26 04:19:54 +0530

    almost_home_mb

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <>
         <h1 className='font-bold text-3xl mt-4 text-teal-500 font-unbounded'>PocKeTracker</h1>
-          <div className='p-2 md:flex gap-10 borde rounded-md border-slate-900 m-auto mt-4 text-slate-400 w-min'>
+          <div className='p-2 md:flex gap-10 borde rounded-md border-slate-900 m-auto mt-4 md:mb-0 mb-10 text-slate-400 w-min'>
           <div className='borde border-opacity-50 bg-slate-900 md:p-2 md:pt-2 md:min-w-[25vw]  bg-slate-00 bg-opacity-20'>
             <Balance/>
             <IncExp/>
